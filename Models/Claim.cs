ddf11a9baedb83a7e9d2516f2365035a5c20f6fe
ST10256824 <130980846+ST10256824@users.noreply.github.com>
2025-09-08 11:50:14 +0000

    Add project files.

---
+++ Models/Claim.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMCS.Models
+
+{
+    public class Claim
+    {
+        public int ClaimID { get; set; }
+
+        [Required]
+        public int LecturerID { get; set; }
+        public Lecturer? Lecturer { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ClaimDate { get; set; } = DateTime.Today;
+
+        [Range(0, 500)]
+        public decimal HoursWorked { get; set; }
+
+        [Range(0, 5000)]
+        public decimal HourlyRate { get; set; }
+
+        // Calculated on the fly; not stored
+        public decimal TotalAmount => Math.Round(HoursWorked * HourlyRate, 2);
+
+        // Pending, Approved, Rejected
+        [Required, StringLength(20)]
+        public string ClaimStatus { get; set; } = "Pending";
+
+        public ICollection<SupportingDocument> SupportingDocuments { get; set; } = new List<SupportingDocument>();
+        public ICollection<Approval> Approvals { get; set; } = new List<Approval>();
+    }
+}
---
+++ Models/Lecturer.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace CMCS.Models
+{
+    public class Lecturer
+    {
+
+        public int LectuerID { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+        public string? ContactNumber {  get; set; }
+
+        public ICollection<Claim> Claims { get; set; } = new List<Claim>();
+    }
+}
